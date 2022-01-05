f6353eb196ef26b864ca3aa4ea0616647d3cacb8
androfficial <material1fox@gmail.com>
2022-01-05 22:15:15 +0200

    Added receiving data in component

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,40 +1,59 @@
+/* eslint-disable camelcase */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import { fetchGame } from '../../store/actions/game';
+import useTypesSelector from '../../hooks/useTypesSelector';
+import { fetchGame, setIsLoaded } from '../../store/actions/game';
 
 const Game = () => {
   const { id } = useParams() as { id: string };
   const dispatch = useDispatch();
+  const [gameDetails, isLoaded] = useTypesSelector(({ game }) => [
+    game.gameDetails,
+    game.isLoaded,
+  ]);
+
+  const { title, thumbnail, game_url, screenshots } = gameDetails;
+  const bgImage = screenshots && screenshots[0].image;
 
   useEffect(() => {
     dispatch(fetchGame(id));
+
+    return () => {
+      dispatch(setIsLoaded(false));
+    };
   }, [dispatch, id]);
 
   return (
-    <section className='page__game game'>
+    <section
+      className='page__game game'
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
       <div className='game__container _container'>
         <div className='game__wrapper'>
           <aside className='game__aside aside-game'>
             <div className='aside-game__box'>
               <div className='aside-game__picture'>
-                <img src='' alt='' />
+                <img src={thumbnail} alt={title} />
               </div>
               <div className='aside-game__body'>
                 <div className='aside-game__btns'>
                   <span className='aside-game__btn-label btn'>FREE</span>
-                  <a href='#' className='aside-game__play-now btn'>
+                  <a href={`${game_url}`} className='aside-game__play-now btn'>
                     PLAY NOW
                   </a>
                 </div>
                 <div className='aside-game__stats'>
-                  <a href='#' className='aside-game__stats-wrapper'>
+                  <button className='aside-game__stats-wrapper' type='button'>
                     {/* svg */}
                     <span className='aside-game__stats-count'>0</span>
                     <span className='aside-game__text'>LIKE</span>
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
