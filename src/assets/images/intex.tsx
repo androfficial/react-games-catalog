18b0b10453d4fac96b0998d36dc75482e17d8701
androfficial <material1fox@gmail.com>
2022-01-06 18:57:03 +0200

    Layout of side block is partially done

---
+++ src/assets/images/intex.tsx
@@ -0,0 +1,8 @@
+export { default as ArrowDown } from './arrow-down.svg';
+export { default as Browser } from './browser.svg';
+export { default as Enter } from './enter.svg';
+export { default as Plus } from './plus.svg';
+export { default as SmileFun } from './smile-fun.svg';
+export { default as SmileMeh } from './smile-meh.svg';
+export { default as SmileSad } from './smile-sad.svg';
+export { default as Windows } from './windows.svg';
--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable camelcase */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import { Plus, SmileFun, SmileMeh, SmileSad } from '../../assets/images/intex';
 import useTypesSelector from '../../hooks/useTypesSelector';
 import { fetchGame, setIsLoaded } from '../../store/actions/game';
 
@@ -28,12 +28,13 @@ const Game = () => {
   }, [dispatch, id]);
 
   return (
-    <section
-      className='page__game game'
-      style={{
-        backgroundImage: `url(${bgImage})`,
-      }}
-    >
+    <section className='page__game game'>
+      <div
+        className='game__background'
+        style={{
+          backgroundImage: `url(${bgImage})`,
+        }}
+      />
       <div className='game__container _container'>
         <div className='game__wrapper'>
           <aside className='game__aside aside-game'>
@@ -43,16 +44,38 @@ const Game = () => {
               </div>
               <div className='aside-game__body'>
                 <div className='aside-game__btns'>
-                  <span className='aside-game__btn-label btn'>FREE</span>
-                  <a href={`${game_url}`} className='aside-game__play-now btn'>
+                  <span className='aside-game__btn aside-game__btn--label btn'>
+                    FREE
+                  </span>
+                  <a
+                    href={`${game_url}`}
+                    target='_blank'
+                    className='aside-game__btn aside-game__btn--play-now btn'
+                    rel='noreferrer'
+                  >
                     PLAY NOW
                   </a>
                 </div>
                 <div className='aside-game__stats'>
                   <button className='aside-game__stats-wrapper' type='button'>
-                    {/* svg */}
+                    <img src={SmileFun} alt='SmileFun icon' />
                     <span className='aside-game__stats-count'>0</span>
                     <span className='aside-game__text'>LIKE</span>
+                  </button>
+                  <button className='aside-game__stats-wrapper' type='button'>
+                    <img src={SmileMeh} alt='SmileMeh icon' />
+                    <span className='aside-game__stats-count'>0</span>
+                    <span className='aside-game__text'>MEH</span>
+                  </button>
+                  <button className='aside-game__stats-wrapper' type='button'>
+                    <img src={SmileSad} alt='SmileSad icon' />
+                    <span className='aside-game__stats-count'>0</span>
+                    <span className='aside-game__text'>DISLIKE</span>
+                  </button>
+                  <button className='aside-game__stats-wrapper' type='button'>
+                    <img src={Plus} alt='Plus icon' />
+                    <span className='aside-game__stats-count'>0</span>
+                    <span className='aside-game__text'>ADD</span>
                   </button>
                 </div>
               </div>
