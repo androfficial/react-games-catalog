2c818bd0aae9d3d609ec24ca484efc03c1853dfe
androfficial <material1fox@gmail.com>
2022-01-12 20:35:00 +0200

    Added 404 error page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-  return <div>NotFound 404</div>;
+  return (
+    <div className='not-found'>
+      <p className='not-found__text'>
+        <strong>Ошибка:</strong>Невозможно показать запрашиваемую страницу.
+        <br />
+        Пожалуйста, попробуйте еще раз позже.
+      </p>
+      <Link to='/games' className='not-found__link btn'>
+        Games Catalog
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
